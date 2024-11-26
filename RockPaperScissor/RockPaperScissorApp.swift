ff563a9d62fe223590a5b0dea92287c5e18384df
Aditya Sharma <adityasharma220803@gmail.com>
2024-11-26 15:26:56 +0530

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Aditya Sharma on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
